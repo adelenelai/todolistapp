44bf5e66c81971b0dc5c80de9838d501de6b87d2
adelenelai <adelenelai@gmail.com>
2023-07-14 14:21:36 +0200

    get app to work

---
+++ src/routes/todolist/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Text from "../../components/Text.svelte";
+    import ToDo from "../../components/ToDo.svelte";
+    export let name = "YOUR_NAME";
+</script>
+
+<h1>To Do List App</h1>
+<p>Here's a simple app for making a To Do List!</p>
+
+<main>
+    <input bind:value={name} />
+
+    <h1>This is {name}'s To Do List!</h1>
+    <Text />
+    <ToDo />
+</main>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.green.100);
+    }
+</style>
